c0ed1c91ab4a246520f0bf9c0a5b90ce519e6de1
yogulvdc95 <55699425+yogulvdc95@users.noreply.github.com>
2019-09-23 15:45:44 +0200

    Add files via upload

---
+++ comienzo/PrimerJava.java
@@ -0,0 +1,13 @@
+package comienzo;
+
+public class PrimerJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Saludo = "hola amigos";
+		
+		System.out.println(Saludo);
+
+	}
+
+}
